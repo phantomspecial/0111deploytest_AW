a787980b4ba2373ed8ba90edbebe79668c5d7a4c
phantomspecial <win.2000@live.jp>
2018-01-17 04:55:53 +0900

    Revise-order-index(delete-helper)

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,8 +1,2 @@
 module OrdersHelper
-
-  # 1注文とその注文に紐付いている注文詳細を取得する
-  def order_orderdetails(order,range)
-    @orderdetails = order.orderdetails.where(created_at: range)
-    return @orderdetails
-  end
 end
