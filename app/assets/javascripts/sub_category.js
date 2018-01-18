0d213a074e88b1ca0439148a9e12ec7e0e2fa92e
phantomspecial <win.2000@live.jp>
2018-01-19 02:06:19 +0900

    Revise-const_var

--- app/assets/javascripts/sub_category.js
+++ app/assets/javascripts/sub_category.js
@@ -1,33 +1,35 @@
-$(document).on('turbolinks:load', function() {
-    const replaceSelectOptions = function($select, results) {
-      $select.html($('<option>'));
-      return $.each(results, function() {
-        const option = $('<option>').val(this.id).text(this.name);
-        return $select.append(option);
-      });
-    };
+$(document).on('turbolinks:load', function () {
+  var replaceSelectOptions = function replaceSelectOptions($select, results) {
+    $select.html($('<option>'));
+    return $.each(results, function () {
+      var option = $('<option>').val(this.id).text(this.name);
+      return $select.append(option);
+    });
+  };
 
-    const replaceChildrenOptions = function() {
-      const { childrenPath } = $(this).find('option:selected').data();
-      const $selectChildren = $(this).closest('form').find('.select-children');
-      if (childrenPath != null) {
-        return $.ajax({
-          url: childrenPath,
-          dataType: "json",
-          success(results) {
-            return replaceSelectOptions($selectChildren, results);
-          },
-          error(XMLHttpRequest, textStatus, errorThrown) {
-            console.error("Error occurred in replaceChildrenOptions");
-            console.log(`XMLHttpRequest: ${XMLHttpRequest.status}`);
-            console.log(`textStatus: ${textStatus}`);
-            return console.log(`errorThrown: ${errorThrown}`);
-          }
-        });
-      } else {
-        return replaceSelectOptions($selectChildren, []);
-      }
-    };
+  var replaceChildrenOptions = function replaceChildrenOptions() {
+    var _$$find$data = $(this).find('option:selected').data(),
+        childrenPath = _$$find$data.childrenPath;
+
+    var $selectChildren = $(this).closest('form').find('.select-children');
+    if (childrenPath != null) {
+      return $.ajax({
+        url: childrenPath,
+        dataType: "json",
+        success: function success(results) {
+          return replaceSelectOptions($selectChildren, results);
+        },
+        error: function error(XMLHttpRequest, textStatus, errorThrown) {
+          console.error("Error occurred in replaceChildrenOptions");
+          console.log('XMLHttpRequest: ' + XMLHttpRequest.status);
+          console.log('textStatus: ' + textStatus);
+          return console.log('errorThrown: ' + errorThrown);
+        }
+      });
+    } else {
+      return replaceSelectOptions($selectChildren, []);
+    }
+  };
 
     return $('.select-parent').on({
     'change': replaceChildrenOptions});
